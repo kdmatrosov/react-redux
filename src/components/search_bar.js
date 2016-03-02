94b1d42573865abf23ba6a8ec6f4cf27e45bd6c9
Kirill Matrosov <root@kPC>
2016-03-02 19:31:10 +0300

    include component to component

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+/**
+ * Created by root on 02.03.16.
+ */
+import React from 'react';
+const SearchBar = () => {
+    return <input/>;
+};
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by root on 02.03.16.
  */
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_YOUTUBE = 'AIzaSyDiHZT2mmAFYQx5e0lX5eVc7m8id81qdOg';
+
+const App = () => {
+    return (<div>
+        <SearchBar/>
+    </div>);
+};
+const container = document.querySelector('.container');
+ReactDom.render(<App/>, container);

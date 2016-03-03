3943f771456fcb20a1ed89616f66e5d2b4b32bb4
Kirill Matrosov <root@kPC>
2016-03-03 17:22:46 +0300

    debounce - вызов функции раз в ... мс

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
@@ -35,9 +36,10 @@ class App extends Component {
 		});
 	}
 	render(){
+		const videoSearch = _.debounce((term)=> {this.videoSearch(term)}, 300);
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<SearchBar onSearchTermChange={videoSearch}/>
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList
 						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
